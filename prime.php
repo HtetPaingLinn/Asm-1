0622d18ebe1d34c7e40f2935f5920a2324befc84
HtetPaingLinn <htetpainglinn2822004@gmail.com>
2024-06-19 23:20:55 +0630

    first commit

--- prime.php
+++ prime.php
@@ -7,12 +7,12 @@
 </head>
 <body>
     <h2>Prime numbers between 1 and 100</h2>
-    <div style="width: 550px; word-wrap: break-word; border: 1px solid #666; font-size: 30px; padding: 20px 40px 20px 40px;">
+    <div style="width: 660px; word-wrap: break-word; border: 1px solid #666; font-size: 30px; padding: 20px 40px 20px 40px;">
         <?php
         $isFirst = true;
         for ($i = 1; $i <= 100; $i++) {
             $isPrime = true;
-            for ($j = 2; $j <= sqrt($i); $j++) {
+            for ($j = 2; $j < $i; $j++) {
                 if ($i % $j == 0) {
                     $isPrime = false;
                     break;
